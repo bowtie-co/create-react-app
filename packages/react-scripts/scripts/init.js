57c06868e1065ef1c3d3c23f842bfa37f50a239c
Charlie McClung <charlie@bowtie.co>
2018-12-17 09:34:30 -0700

    Custom react-scripts

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -218,7 +218,7 @@ module.exports = function(
   const displayedCommand = useYarn ? 'yarn' : 'npm';
 
   console.log();
-  console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(`Success! Created Bowtie react app: ${appName} at ${appPath}`);
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
